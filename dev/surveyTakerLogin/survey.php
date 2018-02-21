bdc6023a21a43b40fcd30e7abe08517f3b5dbac3
Nicholas Estrada <116nic97@gmail.com>
2018-02-20 23:09:06 -0800

    Fixed and finished backend for getting questions on pin login.

---
+++ dev/surveyTakerLogin/survey.php
@@ -0,0 +1,47 @@
+<?php 
+    //include db connection file
+    include_once '../config/database.php';
+
+    sleep(1);
+    
+    // see ../config/database.php for how this works
+    $database = new Database();
+
+    // establishes connection
+    $conn = $database->getConnection();
+    
+    //grabs pin from post data
+    $pin = $_POST["pin"];
+    
+    //creates and executes query to get surveyname, groupname, and account from pins table
+    $sql = "SELECT surveyname, account FROM pins WHERE pin='$pin';";
+    $result = $conn->prepare($sql);
+    $result->execute();
+
+    $numOfRows = $result->rowCount();
+    
+    //sets values for next query
+    if($numOfRows == 1){
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        $surveyname = $row['surveyname'];
+        $account = $row['account'];
+    }
+    else if($numOfRows > 1){
+        die("We messed up. Pins aren't unique");
+        //THIS SHOULD NEVER HAPPEN
+    }
+    else{
+        header("Location: login.php?error=wrongPin");
+        die("Oops! Wrong PIN");
+    }
+      
+    //creates and executes next query to get the questions 
+    $sql = "SELECT * FROM questions WHERE account='$account' AND surveyname='$surveyname';";
+    $result = $conn->prepare($sql);
+    $result.execute();
+
+    //sends info to front-end TODO
+    
+
+    
+        
