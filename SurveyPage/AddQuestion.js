9ba026f9502c2595e1c7a6ffba29899ecc2eba9e
Maxwell Winston <gale1243@gmail.com>
2018-02-22 20:53:11 -0800

    Began work on survey page, added JQuery to directory

---
+++ SurveyPage/AddQuestion.js
@@ -0,0 +1,13 @@
+var checkboxQuestion='<div class="checkbox">\
+        <label class="firstchoice">\
+          <input type="checkbox" class="choice1 choiceCheck" value="A">\
+          </label>\
+          <label id="secondchoice">\
+            <input type="checkbox" class="choice2 choiceCheck" value="B">\
+            </label>\
+            <label id="thirdchoice">\
+              <input type="checkbox" id="choice3" class="choiceCheck" value="C">\
+              </label>\
+              <label id="fourthchoice">\
+                <input type="checkbox" id="choice4" class="choiceRadio" value="D">\
+                </label>\
