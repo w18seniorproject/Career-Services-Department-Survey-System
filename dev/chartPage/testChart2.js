8e29d9eaf1161c00bea72c4faacc0d4bc074d66a
Maxwell Winston <gale1243@gmail.com>
2018-02-24 04:23:23 -0800

    Played around a bit more with graph formatting

--- dev/chartPage/testChart2.js
+++ dev/chartPage/testChart2.js
@@ -10,14 +10,16 @@ var resultChart= new Chart(ctx, {
       label: 'Average Relation Level',
       data: [2,5,4,2],
       backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)'
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
         ]
     }, {
     data: [2,5,4,2],
-
+    backgroundColor: [
+        'rgba(255,255,255, 0.2)',
+      ],
     type: 'line'
   }],
   labels:["Biology","Business", "Computer Science","History"]
