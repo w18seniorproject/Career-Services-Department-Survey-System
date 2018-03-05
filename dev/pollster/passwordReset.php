a87f165fea0be4cc7329c42e60afd06dfe3366e4
Matt <dancinggophers2@yahoo.com>
2018-03-05 13:38:19 -0800

    Formatted 'forgot pword' page, changed direction for functionality

--- dev/pollster/passwordReset.php
+++ dev/pollster/passwordReset.php
@@ -6,7 +6,7 @@ $database = new Database();
 
 $conn = $database->getConnection();
 
-$email = _POST["email"];
+$email = $_POST["email"];
 
 $sql = "SELECT pass, acctName FROM accounts WHERE email = ?;";
 
@@ -21,7 +21,12 @@ if( numRows == 1){
         $pass = $row['pass'];
         $acctName = $row['acctName'];
         $msg = "Your user name is: " . $acctName . "\nYour password is: " .$pass;
-
+    //This might need to be javascript... Get email address from user. Query database to make sure email address is
+    // associated with an account. Send link to email address with hashed (encrypted somehow - hashing every username
+    // to ge a match would be bad...) username as a Query String 
+    //(start by sending unhashed username, just to make sure it works). When link is clicked, open 
+    //"update password" page. Send the same query string along with it, so that the correct account is opened.
+    // On the "update password" page, display the username and provide two password boxes.
         mail( $email, "USS Password Reset", $msg );
     }
     else{
