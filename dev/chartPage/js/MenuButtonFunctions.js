78cb1afb8df95beb56e834536198fdf81265316a
Maxwell Winston <gale1243@gmail.com>
2018-03-17 01:39:17 -0700

    More Experimenting with charts page

---
+++ dev/chartPage/js/MenuButtonFunctions.js
@@ -0,0 +1,13 @@
+function relationsButton(){
+
+  var x=document.getElementById(surveyButtons);
+  if(x.style.display=="none"){
+    x.style.display="block";
+  }else {
+    x.style.display="none";
+  }
+}
+
+function answersButton(){
+
+}
