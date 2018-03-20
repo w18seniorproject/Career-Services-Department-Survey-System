3e5e4eca51a3c679757ace4aa987867d4e16e590
Nicholas Estrada <116nic97@gmail.com>
2018-03-20 12:13:31 -0700

    Started on work parsing post data in pSubmit.php

--- dev/SurveyCreationTesting/pSubmit.php
+++ dev/SurveyCreationTesting/pSubmit.php
@@ -1,4 +1,22 @@
 <?php
+    include_once("../config/pollsterDB.php");
+
+    $database = new Database();
+
+    $conn = $database->getConnection();
+
     $postData = $_POST["dataArray"];
-    echo $postData;
+
+    $sections = explode("\$~\$", $postData);
+
+    $surTitle = $sections[0];
+
+    $toReturn = "";
+
+    foreach($sections as $i){
+        $toReturn .= $i."</br>";
+    }
+
+    echo $toReturn;
+
 ?>
