74b1378ea02d13b32aa3bfbcee38b30ce0ce5fd6
Nicholas Estrada <116nic97@gmail.com>
2018-03-27 13:33:59 -0700

    implemented comments and resources for surveytaker

---
+++ dev/user/resources.js
@@ -0,0 +1,23 @@
+function submitComment(){
+    $.ajax({
+        type: 'POST',
+        url: 'comments.php',
+        cache: false,
+        data: {comment: $("#commentbox").val()},
+        success: function(data){
+            $("#comments").html(data);
+        }
+    })
+}
+
+function showResources(){
+    $.ajax({
+        type: 'POST',
+        url: 'resources.php',
+        cache: false,
+        data: {rLevel : getUrlParameter("rLevel")},
+        success: function(data){
+            $("#resources").html(data);
+        }
+    });
+}
