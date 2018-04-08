8730ba2653478b55eb0005d108e8524899b2d559
Louis A Sorensen <lsorensen@eagles.ewu.edu>
2018-04-07 20:07:19 -0700

    tLogin update
    Updated redirection to proper addresses.

--- dev/SEP/pHp/tLogin.php
+++ dev/SEP/pHp/tLogin.php
@@ -12,11 +12,11 @@
             }
             elseif(isset($survey['message'])){
                 if($survey['message'] === 'notUni'){
-                    header("Location: uLogin.html?error=notUnique");
+                    header("Location: user/uLogin.html?error=notUnique");
                     exit("We messed up. Pins aren't unique");
                     //THIS SHOULD NEVER HAPPEN
                 }elseif($survey['message'] === 'badPin'){
-                    header("Location: uLogin.html?error=wrongPin");
+                    header("Location: user/uLogin.html?error=wrongPin");
                     exit();
                 }
             }
