0ba557f72194033b304acee13cde29320f2aeaad
estradanic <116nic97@gmail.com>
2018-04-29 22:06:34 -0700

    replacing old pDashboard with new version

--- dev/SEP/pollster/pDashboard.js
+++ dev/SEP/pollster/pDashboard.js
@@ -1,50 +1,105 @@
-function showCreateSurvey(){
-    $("#rContainer").html("");
-    $("#rContainer").load("embed.pSurvey.html");
+
+var mobile = false;
+
+function determineLayout(){
+    if($(window).width() < 780){
+        mobile = true;
+    }
+    else{
+        mobile = false;
+        $(window).on('resize', function(){
+            handleResizeNotMobile();
+        });
+        $("body").css({"overflow":"hidden"});
+    }
+    layoutScreen();
+    displaySurveys();
+    if(!mobile){
+        displayRightContent();
+    }
 }
 
-function showMyAccount(){
-    $("#rContainer").html("");
-    $("#rContainer").load("embed.account.html");
+function layoutScreen(){
+    if(mobile){
+        $("body").append(  "<div id='container' style='padding-top: 60px;'>\
+                                <div id='contentHeader' style='height: 45px; padding: 5px !important;'>\
+                                    <input type='button' id='surBtn' class='btn btn-primary btn-sm shadow' style='display: inline-block; margin: 0px; margin-right: 10px;' value='New Survey'>\
+                                    <input type='button' id='acctBtn' class='btn btn-primary btn-sm shadow' style='display: inline-block; margin: 0px; margin-right: 10px;' value='My Account'>\
+                                    <input type='button' id='welcomeBtn' class='btn btn-primary btn-sm shadow' style='float: right;' value='Welcome/Tutorial'>\
+                                </div>\
+                                <div id='contentContainer'></div>\
+                            </div>");
+    }
+    else{
+        $("body").append(  "<div class='dContainer'>\
+                                <div class='leftContent'>\
+                                    <div id='leftContentHeader' class='shadow'>\
+                                        <input type='button' class='btn btn-primary btn-sm shadow' style='display: inline-block; margin: 0px; margin-right: 10px;' onclick='showCreateSurvey()' value='New Survey'>\
+                                        <input type='button' class='btn btn-primary btn-sm shadow' style='display: inline-block; margin: 0px; margin-right: 10px;' onclick='showMyAccount()' value='My Account'>\
+                                        <input type='button' class='btn btn-primary btn-sm shadow' style='float: right;' onclick='showWelcome()' value='Welcome/Tutorial'>\
+                                    </div>\
+                                    <div id='lContainer'></div>\
+                                </div>\
+                                <div class='rightContent'>\
+                                    <div class='rContainer' id='rContainer'></div>\
+                                </div>\
+                            </div>");
+    }
+    $("#surBtn").click(function(){showCreateSurvey()});
+    $("#acctBtn").click(function(){showMyAccount()});
+    $("#welcomeBtn").click(function(){showWelcome()});
 }
 
-function showWelcome(){
-    $("#rContainer").html("<h1>Tutorial and Welcome Stuff will go here</h1>");
+function handleResizeNotMobile(){
+    if($(window).width() < 760){
+        $(".leftContent").css("visibility: hidden;");
+        $(".rightContent").css("width: 100%");
+    }
+    else{
+        $(".leftContent").css("visibility: visible;");
+        if($(window).width()*0.3 < 400){
+            $(".leftContent").width(400);
+            $(".rightContent").width($(window).width() - 401);
+        }
+        else{
+            $(".leftContent").width($(window).width()*0.3);
+            $(".rightContent").width($(window).width()*0.7 - 1);
+        }
+    }
 }
 
-function showManageSurvey(surName){
-    $("#rContainer").html("");
-    $("#rContainer").load("embed.manageSurvey.html", function(){
-        showPinsAndGroupsFilled(surName);
-        showResourcesFilled(surName);
-    });
-}
-
-function showSurveys(){
+function displaySurveys(){
     $.ajax({
         url: "../index.php",
         type: "POST",
         data: ({pdd: "true", aType: "POLL"}),
         success: function(response){
-            if(response.includes("NONE")){
-                $("#lContainer").html(  "<div style='padding-top: 48%; text-align: center;'>\
-                                            <h3 class='greyed-out'>No Surveys</h3>\
-                                            <input type='button' onclick='showCreateSurvey()' value='Create New Survey' class='btn btn-secondary shadow'>\
-                                        </div>");
+            var container;
+            if(mobile){
+                container = $("#contentContainer");
             }
             else{
-                $("#lContainer").html(constructSurveyListHTML(response));
+                container = $("#lContainer");
+            }
+            if(response.includes("NONE")){
+                container.html("<div style='padding-top: 48%; text-align: center;'>\
+                                    <h3 class='greyed-out'>No Surveys</h3>\
+                                    <input type='button' onclick='showCreateSurvey()' value='Create New Survey' class='btn btn-secondary shadow'>\
+                                </div>");
+            }
+            else{
+                container.html(constructSurveyListHTML(response));
                 $(".manage").each(function(i, ele){
-                    $(ele).on("click", function(){displayManageSurvey(ele)});
+                    $(ele).on("click", function(){showManageSurvey($(ele).parent().parent().attr("surName"))});
                 });
                 $(".edit").each(function(i, ele){
-                    $(ele).on("click", function(){displayEditSurvey(ele)});
+                    $(ele).on("click", function(){showEditSurvey($(ele).parent().parent().attr("surName"))});
                 });
                 $(".survey-left").each(function(i, ele){
-                    $(ele).on("click", function(){displayResults(ele)});
+                    $(ele).on("click", function(){showResults($(ele).parent().parent().attr("surName"))});
                 });
                 $(".survey-middle").each(function(i, ele){
-                    $(ele).on("click", function(){displayResults(ele)});
+                    $(ele).on("click", function(){showResults($(ele).parent().parent().attr("surName"))});
                 });
                 $(".surveyListItem").each(function(i, ele){
                     $(ele).on("click", function(){showSelectedItem(ele)});
@@ -55,23 +110,6 @@ function showSurveys(){
             alert("Error: " + error);
         }
     })
-}
-
-function showSelectedItem(ele){
-    $(".surveyListItem-selected").removeClass("surveyListItem-selected");
-    $(ele).addClass("surveyListItem-selected");
-}
-
-function displayResults(ele){
-    //TODO grab database stuff and display results
-}
-
-function displayEditSurvey(ele){
-    //TODO show editsurvey page and prefill from db
-}
-
-function displayManageSurvey(ele){
-    showManageSurvey($(ele).parent().parent().attr("surName"), $(ele).parent().parent().attr("surText"));
 }
 
 function constructSurveyListHTML(surveyJSON){
@@ -91,7 +129,7 @@ function constructSurveyListHTML(surveyJSON){
         var survey = surArr[i];
         var liveIndicatorClass = "live";
         var liveIndicator = "live";
-        if(survey.live === "0"){
+        if(survey.live == "0"){
             liveIndicatorClass = "not-live";
             liveIndicator = "not live";
         }
@@ -116,20 +154,98 @@ function constructSurveyListHTML(surveyJSON){
     return html;
 }
 
-function makeItLookNice(){
-    if($(window).width() < 760){
-        $(".leftContent").css("visibility: hidden;");
-        $(".rightContent").css("width: 100%");
+function displayRightContent(){
+    var contentID = getUrlParameter("content");
+    switch(contentID){
+        case "welcome":
+            showWelcome();
+            break;
+        case "create":
+            showCreateSurvey();
+            break;
+        case "account":
+            showMyAccount();
+            break;
+        case "manage":
+            surName = getUrlParameter("surName");
+            showManageSurvey(surName);
+            break;
+        case "results":
+            surName = getUrlParameter("surName");
+            showResults(surName);
+            break;
+        default:
+            showWelcome();  
+    }
+}
+
+function showSelectedItem(ele){
+    $(".surveyListItem-selected").removeClass("surveyListItem-selected");
+    $(ele).addClass("surveyListItem-selected");
+}
+
+function showCreateSurvey(){
+    if(!mobile){
+        $("#rContainer").html("");
+        $("#rContainer").load("embed.pSurvey.html");
     }
     else{
-        $(".leftContent").css("visibility: visible;");
-        if($(window).width()*0.3 < 400){
-            $(".leftContent").width(400);
-            $(".rightContent").width($(window).width() - 401);
-        }
-        else{
-            $(".leftContent").width($(window).width()*0.3);
-            $(".rightContent").width($(window).width()*0.7 - 1);
-        }
+        $("#contentContainer").html("");
+        $("#contentContainer").load("embed.pSurvey.html");
+    }
+}
+
+function showMyAccount(){
+    if(!mobile){
+        $("#rContainer").html("");
+        $("#rContainer").load("embed.account.html");
+    }
+    else{
+        $("#contentContainer").html("");
+        $("#contentContainer").load("embed.account.html");
+    }
+}
+
+function showWelcome(){
+    if(!mobile){
+        $("#rContainer").html("<h1>Tutorial and Welcome Stuff will go here</h1>");
+    }
+    else{
+        $("#contentContainer").html("<h1>Tutorial and Welcome Stuff will go here</h1>");
+    }
+}
+
+function showManageSurvey(surName){
+    if(!mobile){
+        $("#rContainer").html("");
+        $("#rContainer").load("embed.manageSurvey.html", function(){
+            showPinsAndGroupsFilled(surName);
+            showResourcesFilled(surName);
+        });
+    }
+    else{
+        $("#contentContainer").html("");
+        $("#contentContainer").load("embed.manageSurvey.html", function(){
+            showPinsAndGroupsFilled(surName);
+            showResourcesFilled(surName);
+        });
+    }
+}
+
+function showEditSurvey(surName){
+    if(!mobile){
+        $("#rContainer").html("<h1>Edit Survey will go here</h1>");
+    }
+    else{
+        $("#contentContainer").html("<h1>Edit Survey will go here</h1>");
+    }
+}
+
+function showResults(surName){
+    if(!mobile){
+        $("#rContainer").html("<h1>Results will go here</h1>");
+    }
+    else{
+        $("#contentContainer").html("<h1>Results will go here</h1>");
     }
 }
