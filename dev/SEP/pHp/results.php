5c20b3c367325731c176d46a45b2851e76b7e8b3
estradanic <116nic97@gmail.com>
2018-05-01 10:43:43 -0700

    Began work on results page backend

---
+++ dev/SEP/pHp/results.php
@@ -0,0 +1,39 @@
+<?php
+
+    class Results{
+        
+        public static function GetResults($db){
+            $conn = $db->getConnection('taker');
+
+            $acctName = $_SESSION['userName'];
+            $surName = $_POST['surName'];
+
+            $sql = "SELECT `groupName`, `surResults`, `rLevel`, `time` FROM `results` WHERE `acctName`=? AND `surName`=?;";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(array($acctName, $surName));
+
+            $rNum = $stmt->rowCount();
+
+            if($rNum > 0){
+                $resultsArray = array();
+
+                while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                    extract($row);
+
+                    $result=array(
+                        "groupName" => $groupName,
+                        "surResults" => $surResults,
+                        "rLevel" => $rLevel,
+                        "time" => $time
+                    );
+
+                    $resultsArray[] = $result;
+                }
+                echo json_encode($resultsArray);
+            }
+            else{
+                echo "NONE";
+            }
+        }
+        
+    }
