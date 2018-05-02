1926049ea3914932e0335713af95bc5e6bbcc10d
Maxwell Winston <gale1243@gmail.com>
2018-05-02 11:09:15 -0700

    Moved Stuff TO SEP

---
+++ dev/SEP/chartPage/php/questionNumCallup.php
@@ -0,0 +1,67 @@
+<?php
+  header('Content-Type: application/json');
+
+  $aResult=array();
+
+
+    if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+    if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
+
+     if( !isset($aResult['error']) ){
+
+       switch($_POST['functionname']){
+         case 'questionMenuFiller':
+         {
+           include_once "./config/pollsterDB.php";
+         
+           $database = new Database();
+
+           $conn = $database->getConnection();
+
+           $arguments = $_POST['arguments'];
+
+           $sqlquery = "SELECT qNum FROM question WHERE surName = $arguments ; ";
+           $surQuestions=array();
+
+
+             foreach($conn->quert($sqlquery) as $row){
+               $surQuestions[]=$row['qNum'];
+             }
+
+
+             echo json_encode($surQuestions);
+             break;
+         }
+         case 'pieChartArrayCreator':
+         {
+           include_once "./config/pollsterDB.php";
+
+           $database = new Database();
+
+           $conn = $database->getConnection();
+
+           $arguments = $_POST['arguments'];
+
+           $sqlquery = "SELECT surResults FROM results WHERE surName=$arguments; ";
+           $surQuestions=array();
+
+
+             foreach($conn->quert($sqlquery) as $row){
+               $surQuestions[]=$row['qNum'];
+             }
+
+
+             echo json_encode($surQuestions);
+             break;
+         }
+
+
+       }
+
+
+
+     }
+
+
+ ?>
---
+++ dev/SEP/chartPage/php/relationChartCreator.php
@@ -0,0 +1,27 @@
+
+<?php
+
+  include_once "./config/pollsterDB.php";
+
+  $database = new Database();
+
+  $conn = $database->getConnection();
+
+
+  $sqlquery = "SELECT groupname, ROUND(AVG(relationlevel),0) AS average_relationship FROM results GROUP BY groupname ORDER BY groupname; ";
+  $groupNameArray=array();
+  $relationLevelArray=array();
+
+
+    foreach($conn->quert($sqlquery) as $row){
+      $groupNameArray[]=$row['groupname'];
+      $relationLevelArray[]=$row['average_relationship'];
+    }
+
+    $response=array(
+      'gNA' => $groupNameArray,
+      'rLA' => $relationLevelArray
+    );
+
+    echo json_encode($response)
+ ?>
