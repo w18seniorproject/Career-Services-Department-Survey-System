bec6a128cb7e8f33ba2dea36485374ce9e650b1f
estradanic <116nic97@gmail.com>
2018-05-06 20:20:00 -0700

    All data parsing and getting done in results page. Just need to make it look nice now

--- dev/SEP/pollster/results.js
+++ dev/SEP/pollster/results.js
@@ -22,6 +22,7 @@ function showData(surveyName){
                 results = JSON.parse(data[2]);
                 displayAllGroups();
                 displayGroupButtons();
+                displayQuestionDataAll();
 
             }
         },
@@ -50,36 +51,36 @@ function displayAllGroups(){
 
 function displayGroupButtons(){
     for(var i = 0; i < groupArr.length; i++){
-        var button = "<input value='" + groupArr[i] + "' type='button' style='display: inline-block; margin: 0px; margin-right: 10px;' class='btn btn-secondary btn-sm shadow gb' groupNum='" + (i+1) + "'>";
+        var button = "<input value='" + groupArr[i] + "' type='button' style='display: inline-block; margin: 0px; margin-right: 10px;' class='btn btn-primary btn-sm shadow gb' groupNum='" + (i+1) + "'>";
         $("#groupButtonBar").append(button);
     }
-    var allGroupButton = "<input value='All Groups' type='button' style='display: inline-block; margin: 0px; margin-right: 10px; float: right;' class='btn btn-primary btn-sm shadow gb' groupNum='0'>";
+    var allGroupButton = "<input value='All Groups' type='button' style='display: inline-block; margin: 0px; margin-right: 10px; float: right;' class='btn btn-secondary btn-sm shadow gb' groupNum='0'>";
     $("#groupButtonBar").append(allGroupButton);
     $(".gb").each(function(i, ele){
         $(ele).click(function(){
             displayGroup($(ele).attr("groupNum"));
             $(".gb").each(function(i, element){
-                $(element).removeClass("btn-primary");
-                $(element).addClass("btn-secondary");
+                $(element).removeClass("btn-secondary");
+                $(element).addClass("btn-primary");
             })
-            $(ele).removeClass("btn-secondary");
-            $(ele).addClass("btn-primary");
+            $(ele).removeClass("btn-primary");
+            $(ele).addClass("btn-secondary");
         });
     });
 }
 
-function displayGroup(groupNum){ // DOESN'T WORK YET!!!
+function displayGroup(groupNum){
     if(groupNum == 0){
         displayAllGroups();
         return;
     }
     var timeSD, timeSum=0, num=0, timeAvg, rSum=0, rAvg, timeSumofSquares=0;
     for(var i=0; i<results.length; i++){
-        if(groupArr.indexOf(results[i].groupName)+1 === groupNum){
+        if(groupArr.indexOf(results[i].groupName) === groupNum-1){
             num++;
             timeSum += results[i].time;
-            rSum += results[num].rLevel;
-            timeSumofSquares += results[num].time*results[num].time;    
+            rSum += results[i].rLevel;
+            timeSumofSquares += results[i].time*results[i].time;    
         }
     }
     timeVar = (num * timeSumofSquares - timeSum*timeSum)/(num*num);
@@ -97,3 +98,91 @@ function constructDashDataHTML(rAvg, timeAvg, num, timeSD){
 
     return toReturn;
 }
+
+function displayQuestionDataAll(){
+    for(var i= 0; i < questions.length; i++){
+        var qText = questions[i].qText;
+        var qNum = questions[i].qNum;
+        var qAns = questions[i].qAns;
+        var rLevel = questions[i].rLevel;
+        var qWeight = questions[i].qWeight;
+
+        var response = getResponsesAll(i, questions[i].qType);
+        if(questions[i].qType == "chk"){
+            response = getResponsesCHKAll(i);
+        }
+
+        var appendHTML = constructQuestHTML(qText, qNum, qAns, qWeight, response);
+
+        $("#questionData").append(appendHTML);
+
+    }
+}
+
+function getResponsesAll(qNum, qType){
+    var toReturn = {};
+    switch(qType){
+        case "tf":
+            toReturn = {"t":0, "f":0};
+            break;
+        case "s":
+            toReturn = {"sta":0, "a":0,"sla":0, "sld":0, "d":0, "std":0};
+            break;
+        case "mc":
+            var ans = questions[qNum].qChoices;
+            var choiceArr = ans.split("~$#");
+            for(var i = 0; i < choiceArr.length; i++){
+                toReturn[choiceArr[i].substr(0, -3)] = 0;
+            }
+    }
+
+    for(var i = 0; i < results.length; i++){
+        var surResults = results[i].surResults;
+        rArr = JSON.parse(surResults);
+        rAns = JSON.parse(rArr[qNum]);
+        try{
+            for(var key in toReturn){
+                if(key.includes(rAns.value.substr(0, rAns.value.length-2))){
+                    toReturn[key]++;
+                }
+            }
+        }
+        catch{return toReturn} // try/catch here because not all records will have all questions
+    }
+    return toReturn;
+}
+
+function getResponsesCHKAll(qNum){
+    var ans = questions[qNum].qChoices;
+    var choiceArr = ans.split("~$#");
+    var toReturn = {};
+    for(var i = 0; i < choiceArr.length; i++){
+        var choice = choiceArr[i].substr(0, choiceArr[i].length-3);
+        toReturn[choice] = 0;
+    }
+
+    for(var i = 0; i < results.length; i++){
+        var surResults = results[i].surResults;
+        rArr = JSON.parse(surResults);
+        rAns = JSON.parse(rArr[qNum]);
+        try{
+            for(var key in toReturn){
+                if(key.includes(rAns.value.substr(0, rAns.value.length-2))){
+                    toReturn[key]++;
+                }
+            }
+        }
+        catch{return toReturn} // try/catch here because not all records will have all questions
+    }
+
+    return toReturn;
+}
+
+function constructQuestHTML(qText, qNum, qAns, qWeight, response){
+    var toReturn = "<h3>" + qText + "</h3>";
+    for(var key in response){
+        toReturn += "<h4>" + key + ": " + response[key] + "</h4>";
+    }
+    return toReturn;
+}
+
