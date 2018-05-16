face21488ae45e19be3f4f5107e68293f9f98246
estradanic <116nic97@gmail.com>
2018-05-15 20:13:33 -0700

    notification system 2/3 done

--- dev/SEP/pollster/notifications.js
+++ dev/SEP/pollster/notifications.js
@@ -1,3 +1,24 @@
 function showNotifications(){
-    //TODO query db for notifications and then display them
+    $.ajax({
+        url: "../index.php",
+        type: 'POST',
+        cache: false,
+        data: { aType:'POLL', getNotifications: 'true'},
+        success: function(response){
+            var data = JSON.parse(response);
+            var count = data[0].count;
+            var notifications = data[0].notifications.split("~`#");
+            for(var i = 0; i < notifications.length; i++){
+                displayNotification(notifications[i]);
+            }
+        },
+        error: function(jqxr, status, exception){
+            alert("Failure at showNotifications() ajax call in notifications.js: " + exception);
+        }
+
+    });
+}
+
+function displayNotification(notificationJSON){
+    //TODO parse json and show notification on screen
 }
